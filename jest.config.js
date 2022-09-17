5ea9dbb40bdce7922a3ad1dba3755febeb21de9c
Nicolas Lima <nicolaslima@ext.cvccorp.com.br>
2022-09-17 10:00:08 -0300

    tests: tests configured

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
+};
+
+module.exports = createJestConfig(customJestConfig);
---
+++ src/tests/setupTests.ts
@@ -0,0 +1,5 @@
+import { matchers } from "@emotion/jest";
+import "@testing-library/jest-dom";
+expect.extend(matchers);
+
+export {};
